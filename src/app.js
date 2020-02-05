dab7ca98eb79e542b820a18b16d03fed8d97ca49
anicdao <amielnicdao@gmail.com>
2020-02-05 16:23:02 -0500

    added App.js and index.js content. added Navbar and Jumbotron components

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// app needs to render images upon load
+// each image should have an event handler for clicks
+// should be able to keep track of the user score
+// user gains a point when they click an image they have not clicked  before
+// game ends when user clicks the same image
+// when game ends, alert if they would like to play again
+// record high score
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
