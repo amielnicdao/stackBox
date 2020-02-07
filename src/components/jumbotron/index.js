4b130882371052497a2470e3c88776fcd67632b4
anicdao <amielnicdao@gmail.com>
2020-02-07 16:11:23 -0500

    added jumbotron and navbar content

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import "./style.css";
+
+function Jumbotron(props)
+
+<div className="jumbotron jumbotron-fluid">
+    <div className="container">
+        <h1 className="display-4">stackBox</h1>
+        <p className="lead">You can have any dish on the menu below but only once! If you click on a dish more than once, you lose!</p>
+    </div>
+</div>
+
+export default Jumbotron
